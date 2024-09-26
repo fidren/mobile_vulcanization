3939a35f1c4acca2611696e58ff6d7336a30134d
Wojtek <wojtekmikula1@gmail.com>
2024-09-26 11:10:53 +0200

    changes in date service

--- src/main/java/pl/mobilevulcanization/service/DateService.java
+++ src/main/java/pl/mobilevulcanization/service/DateService.java
@@ -1,18 +1,17 @@
 package pl.mobilevulcanization.service;
 
-import pl.mobilevulcanization.dto.AppointmentDateDto;
 import pl.mobilevulcanization.model.AppointmentDate;
+import pl.mobilevulcanization.request.AddDateRequest;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 
 public interface DateService {
-    AppointmentDate addAppointmentDate(AppointmentDateDto appointmentDateDto);
-    AppointmentDate updateAppointmentDate(AppointmentDateDto appointmentDateDto, LocalDateTime localDateTime);
+    AppointmentDate addAppointmentDate(AddDateRequest addDateRequest);
+    AppointmentDate updateAppointmentDate(String isFreeField, LocalDateTime localDateTime);
     void deleteAppointmentDate(LocalDateTime localDateTime);
-    AppointmentDate getAppointmentDate(LocalDateTime localDateTime);
     List<AppointmentDate> getAllAppointmentDates();
-    List<AppointmentDate> getAllAppointmentDatesByDate(LocalDate date);
-    List<AppointmentDate> getAllCurrentAppointmentDates();
+    List<AppointmentDate> getAllFreeCurrentAppointmentDatesByDate(LocalDate date);
+    List<AppointmentDate> getFilteredDates(LocalDate date, Boolean isCurrent, Boolean isFree);
 }
--- src/main/java/pl/mobilevulcanization/service/DateServiceImpl.java
+++ src/main/java/pl/mobilevulcanization/service/DateServiceImpl.java
@@ -1,13 +1,12 @@
 package pl.mobilevulcanization.service;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
-import pl.mobilevulcanization.dto.AppointmentDateDto;
+import org.springframework.transaction.annotation.Transactional;
 import pl.mobilevulcanization.exception.ResourceNotFoundException;
 import pl.mobilevulcanization.model.AppointmentDate;
-import pl.mobilevulcanization.repository.ClientRepository;
 import pl.mobilevulcanization.repository.DateRepository;
+import pl.mobilevulcanization.request.AddDateRequest;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -19,35 +18,31 @@ public class DateServiceImpl implements DateService{
     private final DateRepository dateRepository;
 
     @Override
-    public AppointmentDate addAppointmentDate(AppointmentDateDto appointmentDateDto) {
-        return dateRepository.save(mapToEntity(appointmentDateDto));
+    public AppointmentDate addAppointmentDate(AddDateRequest addDateRequest) {
+        return dateRepository.save(mapToEntity(addDateRequest));
     }
 
-    private AppointmentDate mapToEntity(AppointmentDateDto appointmentDateDto) {
+    private AppointmentDate mapToEntity(AddDateRequest addDateRequest) {
         return new AppointmentDate(
-                appointmentDateDto.getDate(),
-                appointmentDateDto.isFree()
+                addDateRequest.getDate(),
+                addDateRequest.getIsFree().equals("true")
         );
     }
 
+    @Transactional
     @Override
-    public AppointmentDate updateAppointmentDate(AppointmentDateDto appointmentDateDto, LocalDateTime localDateTime) {
+    public AppointmentDate updateAppointmentDate(String isFreeField, LocalDateTime localDateTime) {
         AppointmentDate appointmentDate = dateRepository.findByDate(localDateTime);
 
         if (appointmentDate == null) {
             throw new ResourceNotFoundException("Date not found");
         }
 
-        appointmentDate = updateExistingDate(appointmentDate, appointmentDateDto);
+        appointmentDate.setFree(isFreeField.equals("true"));
 
         return dateRepository.save(appointmentDate);
     }
 
-    private AppointmentDate updateExistingDate(AppointmentDate existingDate, AppointmentDateDto appointmentDateDto) {
-        existingDate.setDate(appointmentDateDto.getDate());
-        existingDate.setFree(appointmentDateDto.isFree());
-        return existingDate;
-    }
 
     @Override
     public void deleteAppointmentDate(LocalDateTime localDateTime) {
@@ -61,27 +56,18 @@ public class DateServiceImpl implements DateService{
     }
 
     @Override
-    public AppointmentDate getAppointmentDate(LocalDateTime localDateTime) {
-        AppointmentDate appointmentDate = dateRepository.findByDate(localDateTime);
-
-        if (appointmentDate == null) {
-            throw new ResourceNotFoundException("Date: " + localDateTime + " not found");
-        }
-        return appointmentDate;
-    }
-
-    @Override
     public List<AppointmentDate> getAllAppointmentDates() {
         return dateRepository.findAll();
     }
 
     @Override
-    public List<AppointmentDate> getAllAppointmentDatesByDate(LocalDate date) {
-        return dateRepository.findAllByDate(date);
+    public List<AppointmentDate> getAllFreeCurrentAppointmentDatesByDate(LocalDate date) {
+        return dateRepository.findAllFreeCurrentDateByDate(date);
     }
 
     @Override
-    public List<AppointmentDate> getAllCurrentAppointmentDates() {
-        return dateRepository.findAllCurrentDate();
+    public List<AppointmentDate> getFilteredDates(LocalDate date, Boolean isCurrent, Boolean isFree) {
+        return dateRepository.findFilteredDates(date, isCurrent, isFree);
     }
+
 }
